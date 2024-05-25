4f9a530bda3c3165096cccdf9a5518fa3b5c23d2
dgusfr <diegogustavo.fr@gmail.com>
2024-05-24 22:55:49 -0300

    changes for today

--- index.js
+++ index.js
@@ -1,120 +1,117 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(cors());
 
 var DB = {
-    games: [
-        {
-            id: 23,
-            title: "Call of duty MW",
-            year: 2019,
-            price: 60
-        },
-        {
-            id: 65,
-            title: "Sea of thieves",
-            year: 2018,
-            price: 40
-        },
-        {
-            id: 2,
-            title: "Minecraft",
-            year: 2012,
-            price: 20
-        }
-    ]
-}
+  games: [
+    {
+      id: 23,
+      title: "Call of duty MW",
+      year: 2019,
+      price: 60,
+    },
+    {
+      id: 65,
+      title: "Sea of thieves",
+      year: 2018,
+      price: 40,
+    },
+    {
+      id: 2,
+      title: "Minecraft",
+      year: 2012,
+      price: 20,
+    },
+  ],
+};
 
-app.get("/games",(req, res) => {
-    res.statusCode = 200;
-    res.json(DB.games);
+app.get("/games", (req, res) => {
+  res.statusCode = 200;
+  res.json(DB.games);
 });
 
-app.get("/game/:id",(req, res) => {
-    if(isNaN(req.params.id)){
-        res.sendStatus(400);
-    }else{
-        
-        var id = parseInt(req.params.id);
+app.get("/game/:id", (req, res) => {
+  // Verifica se o parâmetro 'id' passado na URL é um número
+  if (isNaN(req.params.id)) {
+    res.sendStatus(400);
+  } else {
+    // Converte o parâmetro 'id' para um número inteiro
+    var id = parseInt(req.params.id);
 
-        var game = DB.games.find(g => g.id == id);
+    var game = DB.games.find((g) => g.id == id);
 
-        if(game != undefined){
-            res.statusCode = 200;
-            res.json(game);
-        }else{
-            res.sendStatus(404);
-        }
+    if (game != undefined) {
+      res.statusCode = 200;
+      res.json(game);
+    } else {
+      res.sendStatus(404);
     }
+  }
 });
 
-app.post("/game",(req, res) => { 
-    var {title, price, year} = req.body;
-    DB.games.push({
-        id: 2323,
-        title,
-        price,
-        year
-    });
-    res.sendStatus(200);
-})
+app.post("/game", (req, res) => {
+  var { title, price, year } = req.body;
+  DB.games.push({
+    id: 2323,
+    title,
+    price,
+    year,
+  });
+  res.sendStatus(200);
+});
 
-app.delete("/game/:id",(req, res) => {
-    if(isNaN(req.params.id)){
-        res.sendStatus(400);
-    }else{
-        var id = parseInt(req.params.id);
-        var index = DB.games.findIndex(g => g.id == id);
+app.delete("/game/:id", (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.sendStatus(400);
+  } else {
+    var id = parseInt(req.params.id);
+    var index = DB.games.findIndex((g) => g.id == id);
 
-        if(index == -1){
-            res.sendStatus(404);
-        }else{
-            DB.games.splice(index,1);
-            res.sendStatus(200);
-        }
+    //Se o index não passou na validação anterior...
+    if (index == -1) {
+      res.sendStatus(404);
+    } else {
+      DB.games.splice(index, 1);
+      res.sendStatus(200);
     }
+  }
 });
 
-app.put("/game/:id",(req, res) => {
+app.put("/game/:id", (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.sendStatus(400);
+  } else {
+    var id = parseInt(req.params.id);
 
-    if(isNaN(req.params.id)){
-        res.sendStatus(400);
-    }else{
-        
-        var id = parseInt(req.params.id);
+    var game = DB.games.find((g) => g.id == id);
 
-        var game = DB.games.find(g => g.id == id);
+    if (game != undefined) {
+      var { title, price, year } = req.body;
 
-        if(game != undefined){
+      if (title != undefined) {
+        game.title = title;
+      }
 
-            var {title, price, year} = req.body;
+      if (price != undefined) {
+        game.price = price;
+      }
 
-            
-            if(title != undefined){
-                game.title = title;
-            }
+      if (year != undefined) {
+        game.year = year;
+      }
 
-            if(price != undefined){
-                game.price = price;
-            }
-
-            if(year != undefined){
-                game.year = year;
-            }
-            
-            res.sendStatus(200);
-
-        }else{
-            res.sendStatus(404);
-        }
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
     }
-
+  }
 });
 
-app.listen(45678,() => {
-    console.log("API RODANDO!");
+app.listen(8000, () => {
+  console.log("API RODANDO!");
 });
