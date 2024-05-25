b63024d94f602b992d68d4b9d21fefa502ee5637
dgusfr <diegogustavo.fr@gmail.com>
2024-05-25 10:09:19 -0300

    add patch and delete on Front

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -10,6 +10,7 @@ function loadGames() {
       games.forEach((game) => {
         var item = document.createElement("li");
 
+        //Definind atributos personalizados, data-id no elemento <li>, que armazenam o ID do game.
         item.setAttribute("data-id", game.id);
         item.setAttribute("data-title", game.title);
         item.setAttribute("data-year", game.year);
@@ -29,6 +30,7 @@ function loadGames() {
           loadForm(item);
         });
 
+        //Adiciona os botões de deletar e editar ao elemento <li>.
         item.appendChild(deleteBtn);
         item.appendChild(editBtn);
 
@@ -43,8 +45,17 @@ function loadGames() {
 // Chama a função loadGames ao carregar a página
 document.addEventListener("DOMContentLoaded", loadGames);
 
-// As outras funções (createGame, deleteGame, loadForm, updateGame) chamam loadGames após realizar a operação
 function createGame() {
+  var titleInput = document.getElementById("title").value;
+  var yearInput = document.getElementById("year").value;
+  var priceInput = document.getElementById("price").value;
+
+  var game = {
+    title: titleInput,
+    year: yearInput,
+    price: priceInput,
+  };
+
   axios
     .post("http://localhost:8000/game", game)
     .then((response) => {
@@ -59,12 +70,14 @@ function createGame() {
 }
 
 function deleteGame(listItem) {
+  var id = listItem.getAttribute("data-id");
+  console.log(id);
   axios
     .delete("http://localhost:8000/game/" + id)
     .then((response) => {
       if (response.status == 200) {
         alert("Game deletado!");
-        loadGames(); // Atualiza a lista de jogos após deletar um jogo
+        loadGames();
       }
     })
     .catch((err) => {
@@ -73,15 +86,39 @@ function deleteGame(listItem) {
 }
 
 function updateGame() {
+  var id = document.getElementById("idEdit").value;
+  var titleInput = document.getElementById("titleEdit").value;
+  var yearInput = document.getElementById("yearEdit").value;
+  var priceInput = document.getElementById("priceEdit").value;
+
+  var game = {
+    title: titleInput,
+    year: yearInput,
+    price: priceInput,
+  };
+
   axios
     .put("http://localhost:8000/game/" + id, game)
     .then((response) => {
       if (response.status == 200) {
         alert("Game atualizado!!");
-        loadGames(); // Atualiza a lista de jogos após editar um jogo
+        loadGames();
       }
     })
     .catch((err) => {
       console.log(err);
     });
+}
+
+// Função para carregar os dados de um game no formulário de edição
+function loadForm(listItem) {
+  var id = listItem.getAttribute("data-id");
+  var title = listItem.getAttribute("data-title");
+  var year = listItem.getAttribute("data-year");
+  var price = listItem.getAttribute("data-price");
+
+  document.getElementById("idEdit").value = id;
+  document.getElementById("titleEdit").value = title;
+  document.getElementById("yearEdit").value = year;
+  document.getElementById("priceEdit").value = price;
 }
